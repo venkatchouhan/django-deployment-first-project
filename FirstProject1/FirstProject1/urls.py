5586e7441729695bd1ad53292981c39637ccf45d
unknown <venkatchouhan>
2022-11-04 16:49:02 +0530

    firstcommit

---
+++ FirstProject1/FirstProject1/urls.py
@@ -0,0 +1,42 @@
+"""FirstProject1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from MyApps1 import views
+from MyApps1 import views as v1
+from MyApps1 import views as f11
+from MyApps1 import views as f22
+from MyApps1 import views as v11;
+from MyApps1 import views as v22;
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('display/',views.display),
+
+    #multiple views
+    path('mrng/', v1.f1),
+    path('aftr/',v1.f2),
+    path('even/',v1.f3),
+
+    path('hello/', f11),
+    path('datetime/', f22),
+
+    path('hello1/', v11.f11),
+    path('datetime1/', v22.f22),
+
+
+]
---
+++ FirstProject1/MyApps1/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+# Create your views here.
+'''
+def display(request):
+    return HttpResponse('<center><h1> hello user welcome to the django first project</h1><hr/>')
+
+
+def f1(request):
+    return HttpResponse("<center><h1 style ='color:red';> Good morning user have a nice day ...!!!!</h1><hr/>");
+def f2(request):                     
+    return HttpResponse("<center><h2 style ='color:orange';> Good afternnon user i hope u doing good ....</h2><hr/>");
+def f3(request):
+    return HttpResponse("<center><h3 style ='color:skyblue';> Good night user have a great day</h3><hr/>");
+'''
+
+def f11(request):
+    return HttpResponse("<center><h1 style ='color:red';> Good morning user have a nice day ...!!!!</h1><hr/>")
+
+
+from django.http import HttpResponse;
+import datetime;
+def f22(request):
+	time = datetime.datetime.now();
+	msg = "<h2 style='color:Green;'>Hello User..!!<br /><br />Server Date & Time :: "+str(time)+"</h2><hr/>"
+	return HttpResponse(msg);
