197c921cece6e6be74c0f50470835a043cb467f1
venkatchouhan <117330752+venkatchouhan@users.noreply.github.com>
2022-11-05 06:07:19 -0700

    Update urls.py

--- FirstProject1/FirstProject1/urls.py
+++ FirstProject1/FirstProject1/urls.py
@@ -25,7 +25,7 @@ from MyApps1 import views as v22;
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
+    path('display/',views.display),
 
     #multiple views
     path('mrng/', v1.f1),
